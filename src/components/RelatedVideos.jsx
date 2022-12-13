0d978e18d3c463846b4fc689e67f4fc57cc5324f
zizpzn <zizpzn@gmail.com>
2022-12-13 16:51:48 +0700

    related videos 1

---
+++ src/components/RelatedVideos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RelatedVideos = ({ id }) => {
+  return <div></div>;
+};
+
+export default RelatedVideos;
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import ChannelInfo from "../components/ChannelInfo";
+import RelatedVideos from "../components/RelatedVideos";
 
 const VideoDetail = () => {
   const {
@@ -8,18 +9,27 @@ const VideoDetail = () => {
   } = useLocation();
   return (
     <section>
-      <iframe
-        id="player"
-        type="text/html"
-        width="100%"
-        height="640"
-        src={`http://www.youtube.com/embed/${video.id}`}
-        frameBorder="0"
-      ></iframe>
-      <div>
-        <h2>{video.snippet.title}</h2>
-        <ChannelInfo id={video.channelId} title={video.channerTitle} />
-      </div>
+      <article>
+        <iframe
+          id="player"
+          type="text/html"
+          width="100%"
+          height="640"
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder="0"
+        ></iframe>
+        <div>
+          <h2>{video.snippet.title}</h2>
+          <ChannelInfo
+            id={video.snippet.channelId}
+            title={video.snippet.channerTitle}
+          />
+          <pre>{video.snippet.description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
     </section>
   );
 };
