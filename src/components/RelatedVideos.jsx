9f0805af5b417238e1f40f843cb462618e399b33
zizpzn <zizpzn@gmail.com>
2022-12-14 17:08:08 +0700

    related videos 2

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "./VideoCard";
 
 const RelatedVideos = ({ id }) => {
-  return <div></div>;
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["related", id], () => youtube.relatedVideos(id));
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😖</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default RelatedVideos;
